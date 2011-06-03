ceeb910e3b7e6e1b45658cf4d3697c6635ae78e4
Junya Ogura <junyaogura@gmail.com>
2011-06-03 14:50:47 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TouchGcal::Application.config.secret_token = '78171735f4d42f29e4f6ca01b7b47334ed09a29e543f511391c8bd1d330e9a83efb9fe49e9f07d6c270c893722a2e4a94c8dc48ee2eff45e7f902ab68065a572'
